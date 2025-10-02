76bc0c0e856582b0e3eb1e0bc4f6568bf9357af8
BhoomiMahna <mahnabhoomi@gmail.com>
2025-10-02 20:55:03 +0530

    Create q1.cpp

---
+++ Assignment 4/Additional/q1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+void printBinaryNumbers(int n) {
+    queue<string> q;
+    q.push("1"); 
+
+    for (int i = 1; i <= n; i++) {
+        string s = q.front();
+        q.pop();
+        cout << s;
+        if (i != n)
+            cout << ", ";
+        q.push(s + "0");
+        q.push(s + "1");
+    }
+    cout << endl;
+}
+
+int main() {
+    int n = 2;
+    printBinaryNumbers(n);
+    return 0;
+}
